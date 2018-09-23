9ba5b21f67ca23b5140d1f920fbd132faa5dd805
Anish Jangra <anish.jangra11@hotmail.com>
2018-09-23 13:38:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+    $.getJSON(url, function (data) {
+
+        var currentQuote = '';
+        var quotes = data.hits;
+        $('.read').on('click', function () {
+            currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+            $('.quoteBody').hide();
+            $('.quoteBodyLink').html(currentQuote.title).attr('href', currentQuote.url).attr('target', '_blank');
+            $('.quoteAuthor').html(currentQuote.author);
+            $('.tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text=' + currentQuote.title + ' By- ' + currentQuote.author).attr('target', '_blank').removeClass('disabled');
+            $('.read').html('Show one more');
+        });
+    });
+});
